5a837b0334bc23ada9941268a8b9c28bdce33228
traininguniverse <treninguniverse@gmail.com>
2022-07-26 12:40:03 +0200

    add fullfill memory

---
+++ DifferentTasks/ZapelnienieSterty/src/net/traininguniverse/zapelnienie_sterty/main/Main.java
@@ -0,0 +1,17 @@
+package net.traininguniverse.zapelnienie_sterty.main;
+
+/*
+ * Przykład przepełnienia pamięci.
+ * 
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		long[] tablica = new long[1000000000];
+
+		// Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+	}
+
+}
